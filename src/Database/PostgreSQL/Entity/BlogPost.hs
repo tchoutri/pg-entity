e32ec22fb86df87c7e7ba41aa78f95b51f779ba9
Théophile Choutri <theophile@choutri.eu>
2021-03-03 00:06:13 +0100

    Add INNER JOIN facilities as well as finer-grained select queries

--- src/Database/PostgreSQL/Entity/BlogPost.hs
+++ src/Database/PostgreSQL/Entity/BlogPost.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE StrictData #-}
 {-|
   Module      : Database.PostgreSQL.Entity.BlogPost
@@ -15,24 +16,48 @@ module Database.PostgreSQL.Entity.BlogPost where
 import Data.Time (UTCTime)
 import Data.UUID (UUID)
 import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Database.PostgreSQL.Entity (Entity (..), insert, withType)
-import Database.PostgreSQL.Entity.DBT.Types (DBT, FromRow, ToRow)
 import Database.PostgreSQL.Simple.FromField (FromField)
+import Database.PostgreSQL.Simple.FromRow (FromRow)
 import Database.PostgreSQL.Simple.ToField (ToField)
+import Database.PostgreSQL.Simple.ToRow (ToRow)
+import Database.PostgreSQL.Transact (DBT)
+import GHC.Records (HasField (..))
 
-newtype BlogPostId
-  = BlogPostId { getBlogPostId :: UUID }
-  deriving newtype (Eq, FromField, Show, ToField)
+import Database.PostgreSQL.Entity (Entity (..), insert, withType)
+
 newtype AuthorId
   = AuthorId { getAuthorId :: UUID }
   deriving newtype (Eq, FromField, Show, ToField)
 
+data Author
+  = Author { authorId  :: AuthorId
+           , name      :: Text
+           , createdAt :: UTCTime
+           }
+  deriving stock (Eq, Generic, Show)
+  deriving anyclass (FromRow, ToRow)
+
+instance HasField x Author a => IsLabel x (Author -> a) where
+  fromLabel = getField @x
+
+instance Entity Author where
+  tableName  = "authors"
+  primaryKey = "author_id"
+  fields     = [ "author_id"
+               , "name"
+               , "created_at"
+               ]
+
+newtype BlogPostId
+  = BlogPostId { getBlogPostId :: UUID }
+  deriving newtype (Eq, FromField, Show, ToField)
 data BlogPost
   = BlogPost { blogPostId :: BlogPostId
                -- ^ Primary key
-             , authorIds  :: Vector AuthorId
+             , authorId   :: AuthorId
                -- ^ Foreign keys, for which we need an explicit type annotation
+             , uuidList   :: Vector UUID
+               -- ^ A type that will need an explicit type annotation in the schema
              , title      :: Text
              , content    :: Text
              , createdAt  :: UTCTime
@@ -40,16 +65,23 @@ data BlogPost
   deriving stock (Eq, Generic, Show)
   deriving anyclass (FromRow, ToRow)
 
+instance HasField x BlogPost a => IsLabel x (BlogPost -> a) where
+  fromLabel = getField @x
+
 instance Entity BlogPost where
   tableName  = "blogposts"
   primaryKey = "blogpost_id"
-  fields = V.fromList [ "blogpost_id"
-                      , "author_ids" `withType` "uuid[]"
-                      , "title"
-                      , "content"
-                      , "created_at"
-                      ]
+  fields = [ "blogpost_id"
+           , "author_id"
+           , "uuid_list" `withType` "uuid[]"
+           , "title"
+           , "content"
+           , "created_at"
+           ]
 
 -- | A specialisation of the 'Database.PostgreSQL.Entity.insert' function.
 insertBlogPost :: BlogPost -> DBT IO ()
 insertBlogPost = insert @BlogPost
+
+insertAuthor :: Author -> DBT IO ()
+insertAuthor = insert @Author
